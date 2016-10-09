61e1037e01a415f119069564ea7fc45ed79b0aec
Rodrigo Webler <rwebler@gmail.com>
2016-10-08 21:13:12 -0300

    Added New Game View

---
+++ Tabletop/Tabletop/GameViewController.swift
@@ -0,0 +1,28 @@
+//
+//  GameViewController.swift
+//  Tabletop
+//
+//  Created by Rodrigo Webler on 10/8/16.
+//  Copyright © 2016 Rodrigo Webler. All rights reserved.
+//
+
+import UIKit
+
+
+class GameViewController : UIViewController {
+    
+    // MARK: Properties
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: Navigation
+    
+}
